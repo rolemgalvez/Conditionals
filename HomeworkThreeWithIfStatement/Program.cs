4b43085782f988789341c3fec9fc98d156b1e96b
Rolem Galvez <rolemgalvez@gmail.com>
2022-12-01 15:05:55 +0800

    Add project files.

---
+++ HomeworkThreeWithIfStatement/Program.cs
@@ -0,0 +1,21 @@
+﻿//Welcome message
+Console.WriteLine("Homework Three with If Statement");
+Console.WriteLine("--------------------------------");
+Console.WriteLine();
+
+//Get user input
+Console.Write("What is your name: ");
+string? name = Console.ReadLine();
+
+//Process results
+Console.WriteLine();
+if (name?.ToLower() == "tim" || name?.ToLower() == "timothy")
+{
+    Console.WriteLine($"Welcome Professor {name}");
+}
+else
+{
+    Console.WriteLine($"Welcome Student {name}");
+}
+
+Console.ReadLine();
---
+++ HomeworkThreeWithSwitchStatement/Program.cs
@@ -0,0 +1,22 @@
+﻿//Welcome message
+Console.WriteLine("Homework Three with Switch Statement");
+Console.WriteLine("------------------------------------");
+Console.WriteLine();
+
+//Get user input
+Console.Write("What is your name: ");
+string? name = Console.ReadLine();
+
+//Process result
+Console.WriteLine();
+switch (name?.ToLower())
+{
+	case "tim" or "timothy":
+		Console.WriteLine($"Welcome Professor {name}");
+		break;
+	default:
+		Console.WriteLine($"Welcome Student {name}");
+		break;
+}
+
+Console.ReadLine();
