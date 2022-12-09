f0b022776d80e98a7db57c56745e20c2806c3260
Rolem Galvez <rolemgalvez@gmail.com>
2022-12-09 11:14:46 +0800

    feature: add HomeworkFour project

---
+++ HomeworkFour/Program.cs
@@ -0,0 +1,29 @@
+﻿//Welcome message
+Console.WriteLine("Homework Four");
+Console.WriteLine("-------------");
+
+//Get user input
+Console.Write("Enter your first name: ");
+string? name = Console.ReadLine();
+
+Console.Write("How about your age? ");
+int.TryParse(Console.ReadLine(), out int age);
+
+//Show result
+Console.WriteLine();
+switch (name?.ToLower())
+{
+	case "bob" or "sue":
+		Console.WriteLine($"Hi Professor {name}!");
+		break;
+	default:
+		Console.WriteLine($"Hello {name}!");
+		break;
+}
+
+if (age < 21)
+{
+	Console.WriteLine($"Recommending you to wait {21-age} year(s) before to start the class.");
+}
+
+Console.ReadLine();
